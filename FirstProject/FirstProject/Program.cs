d34b759315fd60baf89a9cb5b4177a01f7f8531b
iliyanCoding <iliyan.programirane@gmail.com>
2022-04-10 12:54:09 +0300

    FirstProject

---
+++ FirstProject/FirstProject/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace FirstProject
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("This is my first project");
+        }
+    }
+}
