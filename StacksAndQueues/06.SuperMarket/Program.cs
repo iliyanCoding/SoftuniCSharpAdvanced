7fb2b7b159c8f52bfc9f3a41a4c08290b22afff3
iliyanCoding <iliyan.programirane@gmail.com>
2022-05-17 22:24:30 +0300

    Problems for today

---
+++ StacksAndQueues/06.SuperMarket/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace _06.SuperMarket
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Queue<string> queue = new Queue<string>();
+            while (true)
+            {
+                string command = Console.ReadLine();
+                if (command == "Paid")
+                {
+                    while (queue.Count > 0)
+                    {
+                        Console.WriteLine(queue.Dequeue());
+                    }
+                }
+                else if (command == "End")
+                {
+                    Console.WriteLine($"{queue.Count} people remaining.");
+                    break;
+                }
+                else
+                {
+                    queue.Enqueue(command);
+                }
+            }
+        }
+    }
+}
