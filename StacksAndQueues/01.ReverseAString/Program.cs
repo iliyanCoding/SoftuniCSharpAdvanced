31e9b6878c0d09c7b4160246ae4732432995bc84
iliyanCoding <iliyan.programirane@gmail.com>
2022-05-17 22:24:04 +0300

    Problems for today

---
+++ StacksAndQueues/01.ReverseAString/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace _01.ReverseAString
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string input = Console.ReadLine();
+            Stack<char> stack = new Stack<char>();
+            foreach (var ch in input)
+            {
+                stack.Push(ch);
+            }
+            Console.WriteLine(String.Join("", stack));
+        }
+    }
+}
