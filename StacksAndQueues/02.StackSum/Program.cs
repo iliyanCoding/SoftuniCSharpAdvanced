cf0e45406b34f60a88e812bbf287854551847b43
iliyanCoding <iliyan.programirane@gmail.com>
2022-05-17 22:24:08 +0300

    Problems for today

---
+++ StacksAndQueues/02.StackSum/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace _02.StackSum
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] numbers = Console.ReadLine().Split(' ').Select(int.Parse).ToArray();
+            Stack<int> stack = new Stack<int>(numbers);
+            while (true)
+            {
+                string[] command = Console.ReadLine().Split(' ');
+
+                if (command[0].ToLower() == "add")
+                {
+                    stack.Push(int.Parse(command[1]));
+                    stack.Push(int.Parse(command[2]));
+                }
+                else if (command[0].ToLower() == "remove")
+                {
+                    if(stack.Count >= int.Parse(command[1]))
+                    {
+                        for (int i = 0; i < int.Parse(command[1]); i++)
+                        {
+                            stack.Pop();
+                        }
+                    }
+                    
+                }
+                else if (command[0].ToLower() == "end")
+                {
+                    break;
+                }
+            }
+            Console.WriteLine($"Sum: {stack.Sum()}");
+        }
+    }
+}
