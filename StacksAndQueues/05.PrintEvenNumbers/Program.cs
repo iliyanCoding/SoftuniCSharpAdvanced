7438970d94d18a28f5c242b7fa7cd1100527957f
iliyanCoding <iliyan.programirane@gmail.com>
2022-05-17 22:24:24 +0300

    Problems for today

---
+++ StacksAndQueues/05.PrintEvenNumbers/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace _05.PrintEvenNumbers
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] numbers = Console.ReadLine().Split(' ').Select(int.Parse).ToArray();
+            Queue<int> queue = new Queue<int>(numbers);
+            List<int> list = new List<int>();
+            foreach (var num in queue)
+            {
+                if (num % 2 == 0)
+                {
+                    list.Add(num);
+                }
+            }
+            Console.WriteLine(String.Join(", ", list));
+        }
+    }
+}
