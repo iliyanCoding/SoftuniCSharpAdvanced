4f759d177882e80a13722c667cc0b8c4af17a995
iliyanCoding <iliyan.programirane@gmail.com>
2022-05-18 20:59:18 +0300

    The problems for today

---
+++ StacksAndQueuesExercise/03.MaximumAndMinimumElement/Program.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace _03.MaximumAndMinimumElement
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int n = int.Parse(Console.ReadLine());
+            Stack<int> stack = new Stack<int>();
+            for (int i = 0; i < n; i++)
+            {
+                int[] queryArgs = Console.ReadLine().Split(' ', StringSplitOptions.RemoveEmptyEntries).Select(int.Parse).ToArray();
+                if (queryArgs[0] == 1)
+                {
+                    stack.Push(queryArgs[1]);
+                }
+                else if (queryArgs[0] == 2)
+                {
+                    if (stack.Count > 0)
+                    {
+                        stack.Pop();
+                    }
+                }
+                else if (queryArgs[0] == 3)
+                {
+                    if (stack.Count > 0)
+                    {
+                        Console.WriteLine(stack.Max());
+                    }
+                   
+                }
+                else if (queryArgs[0] == 4)
+                {
+                    if (stack.Count > 0)
+                    {
+                        Console.WriteLine(stack.Min());
+                    }
+                    
+                }
+            }
+            Console.WriteLine(string.Join(", ", stack));
+        }
+    }
+}
