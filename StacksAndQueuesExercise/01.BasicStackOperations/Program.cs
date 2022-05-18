7057dede875c44d77e1938b898a9f97348d4a988
iliyanCoding <iliyan.programirane@gmail.com>
2022-05-18 20:59:08 +0300

    The problems for today

---
+++ StacksAndQueuesExercise/01.BasicStackOperations/Program.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace _01.BasicStackOperations
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] input = Console.ReadLine().Split(' ').Select(int.Parse).ToArray();
+            int n = input[0];
+            int s = input[1];
+            int x = input[2];
+            int[] nums = Console.ReadLine().Split(' ').Select(int.Parse).ToArray();
+            Stack<int> stack = new Stack<int>();
+            for (int i = 0; i < n; i++)
+            {
+                stack.Push(nums[i]);
+            }
+            for (int i = 0; i < s; i++)
+            {
+                stack.Pop();
+            }
+            bool xIsFound = false;
+            foreach (var num in stack)
+            {
+                if (num == x)
+                {
+                    xIsFound = true;
+                    Console.WriteLine("true");
+                }
+            }
+            if (xIsFound == false)
+            {
+                if (stack.Count == 0)
+                {
+                    Console.WriteLine("0");
+                }
+                else
+                {
+                    Console.WriteLine(stack.Min());
+                }
+            }
+
+        }
+    }
+}
