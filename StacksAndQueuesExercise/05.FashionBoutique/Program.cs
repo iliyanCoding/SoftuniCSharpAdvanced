fed15a4e7f46c6293cce95024bab3a8985f94ec7
iliyanCoding <iliyan.programirane@gmail.com>
2022-05-18 20:59:28 +0300

    The problems for today

---
+++ StacksAndQueuesExercise/05.FashionBoutique/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace _05.FashionBoutique
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] clothes = Console.ReadLine().Split(' ').Select(int.Parse).ToArray();
+            int rackCapacity = int.Parse(Console.ReadLine());
+            Stack<int> package = new Stack<int>(clothes);
+        }
+    }
+}
