f5238d1a22354cef084f4ffe4ecaaffddfb1dbec
iliyanCoding <iliyan.programirane@gmail.com>
2022-05-22 16:57:55 +0300

    The problems for today

---
+++ MultidimensionalArrays/01.SumMatrixElements/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Linq;
+
+namespace _01.SumMatrixElements
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] dimensions = Console.ReadLine().Split(", ").Select(int.Parse).ToArray();
+
+            int rows = dimensions[0];
+            int columns = dimensions[1];
+
+            int[,] matrix = new int[dimensions[0], dimensions[1]];
+            for (int i = 0; i < matrix.GetLength(0); i++)
+            { 
+                int[] line = Console.ReadLine().Split(", ").Select(int.Parse).ToArray();
+                for (int j = 0; j < matrix.GetLength(1); j++)
+                {
+                    matrix[i, j] = line[j];
+                }
+            }
+
+            int matrixSum = 0;
+            for (int i = 0; i < rows; i++)
+            {
+                for (int j = 0; j < columns; j++)
+                {
+                    matrixSum += matrix[i, j];
+                }
+                
+            }
+
+            Console.WriteLine(rows);
+            Console.WriteLine(columns);
+            Console.WriteLine(matrixSum);
+        }
+    }
+}
