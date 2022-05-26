1d3e9a9d0c3765e57529fb93cf8aacd9b4861c3c
iliyanCoding <iliyan.programirane@gmail.com>
2022-05-26 21:12:17 +0300

    The problems for today

---
+++ SetsAndDictionariesAdvanced/08.SoftuniParty/Program.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace _08.SoftuniParty
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            HashSet<char[]> vipGuests = new HashSet<char[]>();
+            HashSet<char[]> regularGuests = new HashSet<char[]>();
+            while (true)
+            {
+                string input = Console.ReadLine();
+                if (input == "PARTY")
+                {
+                    break;
+                }
+                else
+                {
+                    char[] reservationNumber = input.ToCharArray();
+                    if (Char.IsDigit(reservationNumber[0]))
+                    {
+                        vipGuests.Add(reservationNumber);
+                    }
+                    else
+                    {
+                        regularGuests.Add(reservationNumber);
+                    }
+                }
+            }
+
+            while (true)
+            {
+                string input = Console.ReadLine();
+                if (input == "END")
+                {
+                    break;
+                }
+                else
+                {
+                    char[] reservationNumber = input.ToCharArray();
+                    if (Char.IsDigit(reservationNumber[0]))
+                    {
+                        vipGuests.Remove(reservationNumber);
+                    }
+                    else
+                    {
+                        regularGuests.Remove(reservationNumber);
+                    }
+                }
+            }
+
+            Console.WriteLine(vipGuests.Count + regularGuests.Count);
+            foreach (var vip in vipGuests)
+            {
+                Console.WriteLine(vip);
+            }
+            foreach (var regular in regularGuests)
+            {
+                Console.WriteLine(regular);
+            }
+        }
+    }
+}
