7f3915f26dd49a4cee087b90841caec363bdb897
iliyanCoding <iliyan.programirane@gmail.com>
2022-05-26 21:11:48 +0300

    The problems for today

---
+++ SetsAndDictionariesAdvanced/03.LargestThreeNumbers/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Linq;
+
+namespace _03.LargestThreeNumbers
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] nums = Console.ReadLine().Split(' ').Select(int.Parse).OrderByDescending(n => n).Take(3).ToArray();
+            Console.WriteLine(String.Join(' ', nums));
+        }
+    }
+}
