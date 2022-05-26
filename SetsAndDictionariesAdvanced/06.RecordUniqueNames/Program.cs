69317bfd7c3cc3a1faa88c53d881309166eb3b12
iliyanCoding <iliyan.programirane@gmail.com>
2022-05-26 21:12:07 +0300

    The problems for today

---
+++ SetsAndDictionariesAdvanced/06.RecordUniqueNames/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace _06.RecordUniqueNames
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int n = int.Parse(Console.ReadLine());
+            HashSet<string> setOfNames = new HashSet<string>();
+            for (int i = 0; i < n; i++)
+            {
+                string currName = Console.ReadLine();
+                setOfNames.Add(currName);
+            }
+
+            foreach (var name in setOfNames)
+            {
+                Console.WriteLine(name);
+            }
+        }
+    }
+}
