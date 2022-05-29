e0103451e9292070799fe90032963193f5292b22
iliyanCoding <iliyan.programirane@gmail.com>
2022-05-29 16:09:24 +0300

    Create Program.cs

---
+++ SetsAndDictionariesAdvancedExercise/03.PeriodicTable/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace _03.PeriodicTable
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int n = int.Parse(Console.ReadLine());
+            SortedSet<string> chemicalEl = new SortedSet<string>();
+            for (int i = 0; i < n; i++)
+            {
+                string[] input = Console.ReadLine().Split(" ");
+                for (int h = 0; h < input.Length; h++)
+                {
+                    chemicalEl.Add(input[h]);
+                }
+            }
+            chemicalEl.OrderBy(x => x);
+            Console.WriteLine(string.Join(' ', chemicalEl));
+        }
+    }
+}
