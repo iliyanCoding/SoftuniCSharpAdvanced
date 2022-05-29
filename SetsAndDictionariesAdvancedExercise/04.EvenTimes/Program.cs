1b7b7fb30f2d3983aed2a7215b5f96503005824f
iliyanCoding <iliyan.programirane@gmail.com>
2022-05-29 16:09:30 +0300

    Create Program.cs

---
+++ SetsAndDictionariesAdvancedExercise/04.EvenTimes/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace _04.EvenTimes
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int n = int.Parse(Console.ReadLine());
+            Dictionary<int, int> nums = new Dictionary<int, int>();
+            for (int i = 0; i < n; i++)
+            {
+                int num= int.Parse(Console.ReadLine());
+                if (nums.ContainsKey(num))
+                {
+                    nums[num]++;
+                }
+                else
+                {
+                    nums.Add(num, 1);
+                }
+            }
+            foreach (var kvpNums in nums)
+            {
+                if (kvpNums.Value % 2 == 0)
+                {
+                    Console.WriteLine(kvpNums.Key);
+                }
+            }
+        }
+    }
+}
