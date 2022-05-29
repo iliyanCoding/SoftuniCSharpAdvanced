bb1e60467013d99d1bfb783fbc75b3b72aec06ab
iliyanCoding <iliyan.programirane@gmail.com>
2022-05-29 16:09:35 +0300

    Create Program.cs

---
+++ SetsAndDictionariesAdvancedExercise/05.CountSymbols/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace _05.CountSymbols
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string input = Console.ReadLine();
+            char[] charInput = input.ToCharArray();
+            SortedDictionary<char, int> occurrences = new SortedDictionary<char, int>();
+            for (int i = 0; i < charInput.Length; i++)
+            {
+                char currChar = charInput[i];
+                if (occurrences.ContainsKey(currChar))
+                {
+                    occurrences[currChar]++;
+                }
+                else
+                {
+                    occurrences.Add(currChar, 1);
+                }
+            }
+            
+
+            foreach (var symbol in occurrences)
+            {
+                Console.WriteLine($"{symbol.Key}: {symbol.Value} time/s");
+            }
+        }
+    }
+}
