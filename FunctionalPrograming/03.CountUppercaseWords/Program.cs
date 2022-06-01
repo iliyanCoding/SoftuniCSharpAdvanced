c445233039b09e7356419c67c872cab4fa65b913
iliyanCoding <iliyan.programirane@gmail.com>
2022-06-01 20:44:40 +0300

    Create Program.cs

---
+++ FunctionalPrograming/03.CountUppercaseWords/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Linq;
+
+namespace _03.CountUppercaseWords
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            
+            Predicate<string> isUppercase = s => s[0] == s.ToUpper()[0];
+            string[] input = Console.ReadLine().Split(' ', StringSplitOptions.RemoveEmptyEntries).Where(x => isUppercase(x)).ToArray();
+            foreach (var word in input)
+            {
+                Console.WriteLine(word);
+            }
+        }
+    }
+}
