3fea23b966f2c33a2024d596e196a7d966d0c186
iliyanCoding <iliyan.programirane@gmail.com>
2022-06-01 20:44:28 +0300

    Create Program.cs

---
+++ FunctionalPrograming/01.SortEvenNumbers/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Linq;
+
+namespace _01.SortEvenNumbers
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine(string.Join(", ", Console.ReadLine()
+                .Split(", ")
+                .Select(int.Parse)
+                .Where(x => x % 2 == 0)
+                .OrderBy(x => x)
+                .ToArray()));
+        }
+    }
+}
