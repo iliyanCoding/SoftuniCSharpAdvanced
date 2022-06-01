b67a547d65eda0ed1708dd86f8c59fafe6532879
iliyanCoding <iliyan.programirane@gmail.com>
2022-06-01 20:44:46 +0300

    Create Program.cs

---
+++ FunctionalPrograming/04.AddVAT/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Linq;
+
+namespace _04.AddVAT
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            double[] pricesWithoutVAT = Console.ReadLine().Split(", ").Select(double.Parse).ToArray();
+            Func<double, double> addVAT = x => x * 1.20;
+            foreach (var price in pricesWithoutVAT)
+            {
+                Console.WriteLine($"{addVAT(price):f2}");
+            }
+        }
+    }
+}
