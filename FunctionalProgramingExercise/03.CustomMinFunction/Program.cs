6d1364f12090c77e794f8bca8369aab20d23f39a
iliyanCoding <iliyan.programirane@gmail.com>
2022-06-03 11:43:44 +0300

    Create Program.cs

---
+++ FunctionalProgramingExercise/03.CustomMinFunction/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Linq;
+
+namespace _03.CustomMinFunction
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] nums = Console.ReadLine().Split(' ').Select(int.Parse).ToArray();
+            Console.WriteLine(nums.Min());
+                
+        }
+    }
+}
