6299ba105ae7026a374c14b9fe5efcb8e06ba882
iliyanCoding <iliyan.programirane@gmail.com>
2022-06-03 11:44:01 +0300

    Create Program.cs

---
+++ FunctionalProgramingExercise/05.ApliedArithmetics/Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace _05.ApliedArithmetics
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            List<int> numbers = Console.ReadLine().Split(' ').Select(int.Parse).ToList();
+
+            Func<List<int>, List<int>> add = list => list.Select(number => number += 1).ToList();
+            Func<List<int>, List<int>> multiply = list => list.Select(number => number  *= 2).ToList();
+            Func<List<int>, List<int>> subtract = list => list.Select(number => number  -= 1).ToList();
+            Action<List<int>> print = list => Console.WriteLine(String.Join(' ', list));
+
+            while (true)
+            {
+                string command = Console.ReadLine();
+
+                if (command == "add")
+                {
+                    numbers = add(numbers);
+                }
+                else if (command == "multiply")
+                {
+                    numbers = multiply(numbers);
+                }
+                else if (command == "subtract")
+                {
+                    numbers = subtract(numbers);
+                }
+                else if (command == "print")
+                {
+                    print(numbers);
+                }
+                else if (command == "end")
+                {
+                    break;
+                }
+            }
+        }
+    }
+}
