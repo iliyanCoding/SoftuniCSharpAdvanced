da3ec3312971e28020bf3923133cc9a68a9d953e
iliyanCoding <iliyan.programirane@gmail.com>
2022-06-03 11:43:39 +0300

    Create Program.cs

---
+++ FunctionalProgramingExercise/02.KnightsOfHonour/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace _02.KnightsOfHonour
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] knights = Console.ReadLine().Split(' ');
+            Action<string> printKnights = knight => Console.WriteLine($"Sir {knight}");
+            foreach (var knight in knights)
+            {
+                printKnights(knight);
+            }
+        }
+    }
+}
