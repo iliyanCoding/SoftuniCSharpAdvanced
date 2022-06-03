e1fa629e90f633f4c34bcc5f158b57d5b75886a3
iliyanCoding <iliyan.programirane@gmail.com>
2022-06-03 11:43:50 +0300

    Create Program.cs

---
+++ FunctionalProgramingExercise/04.FindEvenOrOdds/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace _04.FindEvenOrOdds
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string input =Console.ReadLine();
+            int startIndex = int.Parse(input.Split(' ')[0]);
+            int endIndex = int.Parse(input.Split(' ')[1]);
+            List<int> numbers = new List<int>();
+            
+            for (int i = startIndex; i <= endIndex; i++)
+            {
+                numbers.Add(i);
+            }
+
+            string type = Console.ReadLine();
+            Predicate<int> predicate = null;
+            if (type == "even")
+            {
+                predicate = num => num % 2 == 0;
+            }
+            else if (type == "odd")
+            {
+                predicate = num => num % 2 != 0;
+            }
+
+            Console.WriteLine(string.Join(' ', numbers.FindAll(predicate)));
+        }
+    }
+}
