79d56bdf6c513e0dc01c6f39d52aa3f04cb145cc
iliyanCoding <iliyan.programirane@gmail.com>
2022-06-14 18:07:07 +0300

    Create Program.cs

---
+++ FunctionalProgramingExercise/07.PredicateForNames/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace _07.PredicateForNames
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int length = int.Parse(Console.ReadLine());
+
+            Predicate<string> filterByLength = x => x.Length <= length;
+
+            Console.ReadLine()
+                .Split()
+                .Where(s => filterByLength(s))
+                .ToList()
+                .ForEach(Console.WriteLine);
+
+        }
+    }
+}
