4d39acea0466fb0a357e881493e72bdefcc709cf
iliyanCoding <iliyan.programirane@gmail.com>
2022-06-21 20:44:58 +0300

    Create Drone.cs

---
+++ AdvancedExamPrep01/03.Drones/Drone.cs
@@ -0,0 +1,27 @@
+﻿namespace Drones
+{
+    public class Drone
+    {
+        public Drone(string name, string brand, int range)
+        {
+            this.Name = name;
+            this.Brand = brand;
+            this.Range = range;
+        }
+
+        public string Name { get; set; }
+
+        public string Brand { get; set; }
+
+        public int Range { get; set; }
+
+        public bool Available { get; set; } = true;
+
+        public override string ToString()
+        {
+            return $"Drone: {this.Name}\r\n" + 
+                $"Manufactured by: {this.Brand}\r\n" +
+                $"Range: {this.Range} kilometers";
+        }
+    }
+}
