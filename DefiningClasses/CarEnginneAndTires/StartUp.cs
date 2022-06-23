58d6b347f919293ccb34726499efea7c3bc74700
Iliyan Muradilov <102465153+iliyanCoding@users.noreply.github.com>
2022-06-23 09:51:02 +0300

    Create StartUp.cs

---
+++ DefiningClasses/CarEnginneAndTires/StartUp.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace CarManufacturer
+{
+    public class StartUp
+    {
+        static void Main(string[] args)
+        {
+            Tires[] tires = new Tires[4]
+            {
+                new Tires(2000, 2.4),
+                new Tires(2000, 2.5),
+                new Tires(2000, 2.3),
+                new Tires(2000, 2.4)
+            };
+
+            var engine = new Engine(500, 1200);
+
+            var car = new Car("Lamborghini", "Urus", 2010, 250, 9, engine, tires);
+
+        }
+    }
+}
