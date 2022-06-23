113e013f69f6d6b114724b0637839221110cda18
Iliyan Muradilov <102465153+iliyanCoding@users.noreply.github.com>
2022-06-23 09:52:39 +0300

    Create Tire.cs

---
+++ DefiningClasses/CarEnginneAndTires/Tire.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CarManufacturer
+{
+    public class Tires
+    {
+        public Tires(int year, double pressure)
+        {
+            this.Year = year;
+            this.Pressure = pressure;
+        }
+
+        private int year;
+
+        public int Year
+        {
+            get { return year; }
+            set { year = value; }
+        }
+
+        private double pressure;
+
+        public double Pressure
+        {
+            get { return pressure; }
+            set { pressure = value; }
+        }
+
+    }
+}
